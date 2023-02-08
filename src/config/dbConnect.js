0b774705071650cd7d37b88b296bfffde506c5dc
Elian-beep <elianoliver101@gmail.com>
2023-02-07 23:17:15 -0300

    remove envs

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 const dbUser = process.env.DB_USER
 const dbPassword = process.env.DB_PASS
 
-mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@xiloteca-cesit.quv7d2j.mongodb.net/xiloteca`);
+mongoose.connect(`mongodb+srv://kihan:Passarico.09@xiloteca-cesit.quv7d2j.mongodb.net/xiloteca`);
 
 let db = mongoose.connection;
 export default db;
