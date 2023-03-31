1afd305985cc8761cd2433fbe9dee275093adcf8
Elian-beep <elianoliver101@gmail.com>
2023-03-30 22:57:03 -0300

    alternative send img

--- src/controllers/PictureController.js
+++ src/controllers/PictureController.js
@@ -1,6 +1,12 @@
 import Picture from "../models/Picture.js";
 import fs from 'fs';
 
+import { Storage } from '@google-cloud/storage';
+import path, { dirname } from "path";
+import { v4 as uuidv4 } from 'uuid';
+import { fileURLToPath } from "url";
+import { json } from "express";
+
 class PicturesController {
 
     static async create(req, res) {
@@ -12,13 +18,46 @@ class PicturesController {
                 name,
                 src: file.path
             });
-
             await picture.save()
             return res.json({picture, msg: "Imgem salva com sucesso!"})
         }catch(error){
             return res.status(500).json({message: "Erro ao salvar imagem.", error: error.message});
         }
     }
+
+    /*
+    static async create(req, res, next){
+        const storage = new Storage({
+            keyFilename: path.join(dirname(fileURLToPath(import.meta.url)), './data/pictures-xiloteca-547296c599c1.json'),
+            projectId: 'pictures-xiloteca'
+        });
+        const bucket = storage.bucket('pictures-xiloteca');
+
+        const file = req.file;
+        const blobName = (req, file, cb) => cb(null, Date.now() + path.extname(file.originalname));
+        const blob = bucket.file(blobName);
+
+        if (req.file && req.file.mimetype) {
+            const stream = blob.createWriteStream({
+                metadata: {
+                    contentType: file.mimetype
+                }
+            });
+        }else{
+            return res.json({msg: "arquivo nulo"});
+        }
+
+        stream.on('error', (err) => {
+            next(err);
+        });
+        
+        stream.on('finish', () => {
+            const publicUrl = `https://storage.googleapis.com/${bucket.name}/${blobName}`;
+            res.status(200).send(publicUrl);
+        });
+        stream.end(file.buffer);
+    }
+    */
 
     static async findAll(req, res){
         try{
