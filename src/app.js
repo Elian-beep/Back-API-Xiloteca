c2998cb5f5b6e92e6e5dda1f1d2eb4841aee1946
Elian-beep <elianoliver101@gmail.com>
2023-06-13 16:47:58 -0300

    :bug: perf: concat links

--- src/app.js
+++ src/app.js
@@ -13,6 +13,10 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'https://xilotecacesi.vercel.app, https://xilotecauea.vercel.app');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+
+    if (req.headers.origin === 'https://xilotecauea.vercel.app') {
+        res.header('Access-Control-Allow-Methods', 'GET');
+    }
     next();
 });
 app.use(express.json())
