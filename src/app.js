a95fac2d70b2a337584f536e358e33c9f04dc6b8
Elian-beep <elianoliver101@gmail.com>
2023-06-13 16:36:16 -0300

    :racehorse: perf: config complete in cors

--- src/app.js
+++ src/app.js
@@ -9,7 +9,12 @@ db.once("open", () => {
 })
 
 const app = express();
-app.use(cors());
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://xilotecacesi.vercel.app, https://xilotecauea.vercel.app');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
 app.use(express.json())
 routes(app);
 
